caae459ef2965c6c4a2606377e16a4a946c1bfc8
Anna Cai <annacai44@gmail.com>
2023-10-03 14:54:22 -0500

    add spacing between term selectors and fix formatting

--- src/components/TermSelector/TermSelector.jsx
+++ src/components/TermSelector/TermSelector.jsx
@@ -1,31 +1,40 @@
-import { useState } from "react";
-
 const terms = ["Fall", "Winter", "Spring"];
 
-const MenuButton = ({term, selection, setSelection}) => (
+const MenuButton = ({ term, selection, setSelection }) => (
   <div>
-    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
-      onChange={() => setSelection(term)} />
-    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
-    { term }
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      checked={term === selection}
+      autoComplete="off"
+      onChange={() => setSelection(term)}
+    />
+    <label className="btn btn-success mb-1 p-2 m-1" htmlFor={term}>
+      {term}
     </label>
   </div>
 );
 
-const MenuSelector = ({selection, setSelection}) => (
+const MenuSelector = ({ selection, setSelection }) => (
   <div className="btn-group">
-    { 
-      terms.map(term => <MenuButton key={term} term={term} selection={selection} setSelection={setSelection} />)
-    }
+    {terms.map((term) => (
+      <MenuButton
+        key={term}
+        term={term}
+        selection={selection}
+        setSelection={setSelection}
+      />
+    ))}
   </div>
 );
 
-const TermSelector = ({term, setTerm}) => {
+const TermSelector = ({ term, setTerm }) => {
   return (
     <div>
       <MenuSelector selection={term} setSelection={setTerm} />
     </div>
   );
-}
+};
 
 export default TermSelector;
