c63bf445977a3dd9f15635f7512c2aaa79c92bc6
Anna Cai <annacai44@gmail.com>
2023-10-12 15:28:40 -0500

    fix course form

--- src/components/CourseForm/CourseForm.jsx
+++ src/components/CourseForm/CourseForm.jsx
@@ -1,39 +1,97 @@
-import { Form, Button } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
-import "./CourseForm.css";
+import { useFormData } from "../../utilities/useFormData";
+import { useNavigate, useParams } from "react-router-dom";
+const validateUserData = (key, val) => {
+  switch (key) {
+    case "firstName":
+    case "lastName":
+      return /(^\w\w)/.test(val) ? "" : "must be least two characters";
+    case "email":
+      return /^\w+@\w+[.]\w+/.test(val)
+        ? ""
+        : "must contain name@domain.top-level-domain";
+    default:
+      return "";
+  }
+};
 
-const onSubmit = () => {};
+const InputField = ({ name, text, state, change }) => (
+  <div className="mb-3">
+    <label htmlFor={name} className="form-label">
+      {text}
+    </label>
+    <input
+      className="form-control"
+      id={name}
+      name={name}
+      defaultValue={state.values?.[name]}
+      onChange={change}
+    />
+    <div className="invalid-feedback">{state.errors?.[name]}</div>
+  </div>
+);
+
+const ButtonBar = ({ message, disabled }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="d-flex">
+      <button
+        type="button"
+        className="btn btn-outline-dark me-2"
+        onClick={() => navigate(-1)}
+      >
+        Cancel
+      </button>
+      {/* <button
+        type="submit"
+        className="btn btn-primary me-auto"
+        disabled={disabled}
+      >
+        Submit
+      </button> */}
+      <span className="p-2">{message}</span>
+    </div>
+  );
+};
 
 const CourseForm = ({ courses }) => {
+  //   const [update, result] = useDbUpdate(`/users/${user.id}`);
   const { courseId } = useParams();
   const course = Object.entries(courses).filter(
     ([id, courseData]) => courseId === id
   )[0];
-
   const courseData = course[1];
-  return (
-    <Form onSubmit={onSubmit}>
-      <Form.Group className="mb-3" controlId="formCourseTitle">
-        <Form.Label>Course Title</Form.Label>
-        <Form.Control
-          defaultValue={courseData.title}
-          placeholder="Enter course title"
-        />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formMeetingTimes">
-        <Form.Label>Meeting Times</Form.Label>
-        <Form.Control
-          defaultValue={courseData.meets}
-          placeholder="Enter meeting times"
-        />
-      </Form.Group>
-      <Link to={`/`}>
-        <Button variant="secondary" type="cancel">
-          Cancel
-        </Button>
-      </Link>
-    </Form>
+  const [state, change] = useFormData(validateUserData, {
+    courseTitle: courseData.title,
+    meetingTimes: courseData.meets,
+  });
+  const submit = (evt) => {
+    evt.preventDefault();
+    if (!state.errors) {
+      update(state.values);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={submit}
+      noValidate
+      className={state.errors ? "was-validated" : null}
+    >
+      <InputField
+        name="courseTitle"
+        text="Course Title"
+        state={state}
+        change={change}
+      />
+      <InputField
+        name="meetingTimes"
+        text="Meeting Times"
+        state={state}
+        change={change}
+      />
+      <ButtonBar message={""} />
+    </form>
   );
 };
 
