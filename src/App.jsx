c7a2e417e33560cde350f3390f3e8d0dd9099ad9
Anna Cai <annacai44@gmail.com>
2023-10-12 20:40:31 -0500

    add 'loading user data' when data is still being fetched

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ const Main = () => {
   const [data, error] = useDbData("/");
 
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  // if (isLoading) return <h1>Loading user data...</h1>;
-  if (!data) return <h1>No user data found</h1>;
+  if (!data) return <h1>Loading user data...</h1>;
 
   return (
     <div>
